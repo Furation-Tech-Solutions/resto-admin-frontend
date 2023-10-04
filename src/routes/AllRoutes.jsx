7c29c85051d3bfa545099d9cfb6e003bec227610
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-10-04 11:30:17 +0530

    added forgot password page

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -4,16 +4,16 @@ import Home from '../pages/Home'
 import Contact from '../pages/Contact'
 import About from '../pages/About'
 import Login from '../pages/Login'
-import Register from '../pages/Register'
 import AdminDashboard from '../pages/AdminDashboard'
 import Admin from '../pages/Admin'
+import Forgotpassword from '../pages/Forgotpassword'
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/login' element={<Login/>}></Route>
-      <Route path='/register' element={<Register/>}></Route>
+      <Route path='/forgotpassword' element={<Forgotpassword/>}></Route>
       <Route path='/about' element={<About/>}></Route>
       <Route path='/contact' element={<Contact/>}></Route>
       <Route path='/admindashboard' element={<AdminDashboard/>}></Route>
