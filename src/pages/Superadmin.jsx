7f6c090590ac64f5b4ea1bec2b44c583ec2af9fc
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-10-09 18:27:24 +0530

    added superadmin page support

--- src/pages/Superadmin.jsx
+++ src/pages/Superadmin.jsx
@@ -155,143 +155,143 @@ const Superadmin = () => {
 
   return (
     <div className="outerBoxSuperAdmin">
-    <div className="innerBoxSuperAdmin">
-      <div className="leftBoxSuperAdmin">
-        <div className="leftfirstBoxSuperAdmin">
-          <img src={logo} alt="applogo" />
-          <p>Furation Tech</p>
-        </div>
-        <div className="leftsecondBoxSuperAdmin">
-          <div
-            onClick={() => setBarnum(1)}
-            className={
-              barnum === 1 ? "leftsecondfirstSuperAdminOn" : "leftsecondfirstSuperAdmin"
-            }
-          >
-            <p>Dashboard</p>
+      <div className="innerBoxSuperAdmin">
+        <div className="leftBoxSuperAdmin">
+          <div className="leftfirstBoxSuperAdmin">
+            <img src={logo} alt="applogo" />
+            <p>Furation Tech</p>
           </div>
-          <div
-            onClick={() => setBarnum(2)}
-            className={
-              barnum === 2 ? "leftsecondthirdSuperAdminOn" : "leftsecondthirdSuperAdmin"
-            }
-          >
-            <p>Support</p>
-          </div>
-        </div>
-        <div className="leftthirdBoxSuperAdmin">
-          <div className="leftthirdBoxfirstSuperAdmin">Logout</div>
-        </div>
-      </div>
-      <div className='rightBoxSuperAdmin'>
-        <div className={barnum===1 ? 'rightBoxSuperAdminDashboard' : 'rightBoxSuperAdminDashboardOff'}>
-          <div className='rightfirstBoxSuperadmin'>
-            <div className='rightfirstBoxSuperadminText'>
-              <p>Hello, Super Admin!</p>
-              <p>13 June, 2023 Tuesday</p>
+          <div className="leftsecondBoxSuperAdmin">
+            <div
+              onClick={() => setBarnum(1)}
+              className={
+                barnum === 1 ? "leftsecondfirstSuperAdminOn" : "leftsecondfirstSuperAdmin"
+              }
+            >
+              <p>Dashboard</p>
             </div>
-            <div>
-              <button onClick={()=>setIsAddAdmin(true)} className="addadminbuttonSuperadmin"><AiOutlinePlus/>Add Admin</button>
+            <div
+              onClick={() => setBarnum(2)}
+              className={
+                barnum === 2 ? "leftsecondthirdSuperAdminOn" : "leftsecondthirdSuperAdmin"
+              }
+            >
+              <p>Support</p>
             </div>
           </div>
-          <div className='rightsecondBoxSuperadmin'>
-            <div className="rightsecondBoxSuperadminInputBox">
-              <AiOutlineSearch size={"20px"} />
-              <input className="rightsecondBoxSuperadminInput" type="text" placeholder='Search by business name, email or phone number' />
-            </div>
-            <select className="rightsecondBoxSuperadminSelect" name="" id="">
-              <option value="">Filter By</option>
-              <option value="">Akina</option>
-              <option value="">Pritam da dhaba</option>
-            </select>
+          <div className="leftthirdBoxSuperAdmin">
+            <div className="leftthirdBoxfirstSuperAdmin">Logout</div>
           </div>
-          <div className='rightthirdBoxSuperadmin'>
-            <table className="userTableSuperAdmin">
-              <tr>
-                <th className="userTableHeadSuperAdmin">Sr. No.</th>
-                <th className="userTableHeadSuperAdmin">Business Name</th>
-                <th className="userTableHeadSuperAdmin">Email Address</th>
-                <th className="userTableHeadSuperAdmin">Phone Number</th>
-                <th className="userTableHeadSuperAdmin">Subscription</th>
-                <th className="userTableHeadSuperAdmin">Action</th>
-              </tr>
-              {userData && userData.map((user, i)=>{
-                return <tr>
-                  <td className="userTableBodySuperAdmin">{i+1}</td>
-                  <td className="userTableBodySuperAdmin">{user.businessName}</td>
-                  <td className="userTableBodySuperAdmin">{user.email}</td>
-                  <td className="userTableBodySuperAdmin">{user.phone}</td>
-                  <td className="userTableBodySuperAdmin">{user.subsciption}</td>
-                  <td className="userTableBodySuperAdmin">Edit</td>
+        </div>
+        <div className='rightBoxSuperAdmin'>
+          <div className={barnum===1 ? 'rightBoxSuperAdminDashboard' : 'rightBoxSuperAdminDashboardOff'}>
+            <div className='rightfirstBoxSuperadmin'>
+              <div className='rightfirstBoxSuperadminText'>
+                <p>Hello, Super Admin!</p>
+                <p>13 June, 2023 Tuesday</p>
+              </div>
+              <div>
+                <button onClick={()=>setIsAddAdmin(true)} className="addadminbuttonSuperadmin"><AiOutlinePlus/>Add Admin</button>
+              </div>
+            </div>
+            <div className='rightsecondBoxSuperadmin'>
+              <div className="rightsecondBoxSuperadminInputBox">
+                <AiOutlineSearch size={"20px"} />
+                <input className="rightsecondBoxSuperadminInput" type="text" placeholder='Search by business name, email or phone number' />
+              </div>
+              <select className="rightsecondBoxSuperadminSelect" name="" id="">
+                <option value="">Filter By</option>
+                <option value="">Akina</option>
+                <option value="">Pritam da dhaba</option>
+              </select>
+            </div>
+            <div className='rightthirdBoxSuperadmin'>
+              <table className="userTableSuperAdmin">
+                <tr>
+                  <th className="userTableHeadSuperAdmin">Sr. No.</th>
+                  <th className="userTableHeadSuperAdmin">Business Name</th>
+                  <th className="userTableHeadSuperAdmin">Email Address</th>
+                  <th className="userTableHeadSuperAdmin">Phone Number</th>
+                  <th className="userTableHeadSuperAdmin">Subscription</th>
+                  <th className="userTableHeadSuperAdmin">Action</th>
                 </tr>
-              })}
-            </table>
-          </div>
-        </div>
-        <div className={barnum===2 ? 'rightBoxSuperAdminSupport' : 'rightBoxSuperAdminSupportOff'}>
-          <div className='rightBoxSuperAdminSupportText'><p>Support</p></div>
-          <div className='rightsecondBoxSuperadmin'>
-            <div className="rightsecondBoxSuperadminInputBox">
-              <AiOutlineSearch size={"20px"} />
-              <input className="rightsecondBoxSuperadminInput" type="text" placeholder='Search by business name, email or phone number' />
+                {userData && userData.map((user, i)=>{
+                  return <tr>
+                    <td className="userTableBodySuperAdmin">{i+1}</td>
+                    <td className="userTableBodySuperAdmin">{user.businessName}</td>
+                    <td className="userTableBodySuperAdmin">{user.email}</td>
+                    <td className="userTableBodySuperAdmin">{user.phone}</td>
+                    <td className="userTableBodySuperAdmin">{user.subsciption}</td>
+                    <td className="userTableBodySuperAdmin">Edit</td>
+                  </tr>
+                })}
+              </table>
             </div>
-            <select className="rightsecondBoxSuperadminSelect" name="" id="">
-              <option value="">Filter By</option>
-              <option value="">Akina</option>
-              <option value="">Pritam da dhaba</option>
-            </select>
           </div>
-          <div className='rightthirdBoxSuperadmin'>
-            <table className="feedbackTableSuperAdmin">
-              <tr>
-                <th className="feedbackTableHeadSuperAdmin">Sr. No.</th>
-                <th className="feedbackTableHeadSuperAdmin">Name</th>
-                <th className="feedbackTableHeadSuperAdmin">Phone Number</th>
-                <th className="feedbackTableHeadSuperAdmin">Feedback</th>
-                <th className="feedbackTableHeadSuperAdmin">Date</th>
-              </tr>
-              {feedbackData && feedbackData.map((feedback, i)=>{
-                return <tr>
-                  <td className="feedbackTableBodySuperAdmin">{i+1}</td>
-                  <td className="feedbackTableBodySuperAdmin">{feedback.name}</td>
-                  <td className="feedbackTableBodySuperAdmin">{feedback.phone}</td>
-                  <td className="feedbackTableBodySuperAdmin">{feedback.message}</td>
-                  <td className="feedbackTableBodySuperAdmin">{feedback.date}</td>
+          <div className={barnum===2 ? 'rightBoxSuperAdminSupport' : 'rightBoxSuperAdminSupportOff'}>
+            <div className='rightBoxSuperAdminSupportText'><p>Support</p></div>
+            <div className='rightsecondBoxSuperadmin'>
+              <div className="rightsecondBoxSuperadminInputBox">
+                <AiOutlineSearch size={"20px"} />
+                <input className="rightsecondBoxSuperadminInput" type="text" placeholder='Search by business name, email or phone number' />
+              </div>
+              <select className="rightsecondBoxSuperadminSelect" name="" id="">
+                <option value="">Filter By</option>
+                <option value="">Akina</option>
+                <option value="">Pritam da dhaba</option>
+              </select>
+            </div>
+            <div className='rightthirdBoxSuperadmin'>
+              <table className="feedbackTableSuperAdmin">
+                <tr>
+                  <th className="feedbackTableHeadSuperAdmin">Sr. No.</th>
+                  <th className="feedbackTableHeadSuperAdmin">Name</th>
+                  <th className="feedbackTableHeadSuperAdmin">Phone Number</th>
+                  <th className="feedbackTableHeadSuperAdmin">Feedback</th>
+                  <th className="feedbackTableHeadSuperAdmin">Date</th>
                 </tr>
-              })}
-            </table>
+                {feedbackData && feedbackData.map((feedback, i)=>{
+                  return <tr>
+                    <td className="feedbackTableBodySuperAdmin">{i+1}</td>
+                    <td className="feedbackTableBodySuperAdmin">{feedback.name}</td>
+                    <td className="feedbackTableBodySuperAdmin">{feedback.phone}</td>
+                    <td className="feedbackTableBodySuperAdmin">{feedback.message}</td>
+                    <td className="feedbackTableBodySuperAdmin">{feedback.date}</td>
+                  </tr>
+                })}
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className={ isAddAdmin? "addadmin" : "addadminOff"}>
-      <p className='addadmintext'>Add a new admin</p>
-      <div>
-        <label className='addadminlabel'>Business Name</label><br/>
-        <input className='addadmininput' type="text" placeholder='Name a business' /><br/>
-        <label className='addadminlabel'>Email</label><br/>
-        <input className='addadmininput' type="email" placeholder='Enter email' /><br/>
-        <label className='addadminlabel'>Phone</label><br/>
-        <input className='addadmininput' type="number" placeholder='Enter phone number' /><br/>
-        <label className='addadminlabel'>Set a password</label><br/>
-        <input className='addadmininput' type="text" placeholder='Enter password' /><br/>
+      <div className={ isAddAdmin? "addadmin" : "addadminOff"}>
+        <p className='addadmintext'>Add a new admin</p>
+        <div>
+          <label className='addadminlabel'>Business Name</label><br/>
+          <input className='addadmininput' type="text" placeholder='Name a business' /><br/>
+          <label className='addadminlabel'>Email</label><br/>
+          <input className='addadmininput' type="email" placeholder='Enter email' /><br/>
+          <label className='addadminlabel'>Phone</label><br/>
+          <input className='addadmininput' type="number" placeholder='Enter phone number' /><br/>
+          <label className='addadminlabel'>Set a password</label><br/>
+          <input className='addadmininput' type="text" placeholder='Enter password' /><br/>
+        </div>
+        <button className='addadminbutton'>Add Admin</button><br/>
+        <button onClick={()=>setIsAddAdmin(false)} className='addadmincancelbutton'>Cancel</button>
       </div>
-      <button className='addadminbutton'>Add Admin</button><br/>
-      <button onClick={()=>setIsAddAdmin(false)} className='addadmincancelbutton'>Cancel</button>
-    </div>
-    <div className={ istoggle? "togglesubscription" : "togglesubscriptionOff"}>
-      <p className='togglesubscriptiontext'>Are you sure you want to deactivate the subsciption?</p>
-      <div>
-        <label className='togglesubscriptionlabel'>Type DEACTIVATE to confirm</label><br/>
-        <input className='togglesubscriptioninput' type="text" /><br/>
-        <p className='togglesubscriptionerror'>Sorry, please enter the text exactly as displayed to confirm.</p>
-      </div>
-      <div className='togglesubscriptionbuttonBox'>
-        <button className='togglesubscriptionbutton'>Deactivate</button><br/>
-        <button onClick={()=>setIstoggle(false)} className='togglesubscriptioncancelbutton'>Cancel</button>
+      <div className={ istoggle? "togglesubscription" : "togglesubscriptionOff"}>
+        <p className='togglesubscriptiontext'>Are you sure you want to deactivate the subsciption?</p>
+        <div>
+          <label className='togglesubscriptionlabel'>Type DEACTIVATE to confirm</label><br/>
+          <input className='togglesubscriptioninput' type="text" /><br/>
+          <p className='togglesubscriptionerror'>Sorry, please enter the text exactly as displayed to confirm.</p>
+        </div>
+        <div className='togglesubscriptionbuttonBox'>
+          <button className='togglesubscriptionbutton'>Deactivate</button><br/>
+          <button onClick={()=>setIstoggle(false)} className='togglesubscriptioncancelbutton'>Cancel</button>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
