b0c179bc87395360d728bf116bb8f026c0251ebf
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-11-02 11:19:25 +0530

    updated login url

--- src/Redux/AuthData/action.js
+++ src/Redux/AuthData/action.js
@@ -4,7 +4,7 @@ import * as types from"./actionTypes";
 
 const login= (payload) => (dispatch) => {
     dispatch({type: types.USER_LOGIN_REQUEST});
-    return axios.post("https://restaurant-bot-admin.onrender.com/api/v1/user/login", payload)
+    return axios.post("https://restaurant-bot-admin.onrender.com/api/v1/admin/login", payload)
     .then((r)=>{
         localStorage.setItem("token", JSON.stringify(r.data.token));
         dispatch({type: types.USER_LOGIN_SUCCESS, payload: {token: r.data.token,role: r.data.role}})
